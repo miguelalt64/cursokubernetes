19b4a14d5dd74a8b6afa6e11bf5d7a41199810c7
miguelalt64 <48498631+miguelalt64@users.noreply.github.com>
2024-12-30 00:45:25 -0600

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,26 @@
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sudo sysctl --system
+sudo apt-get update && sudo apt-get install -y containerd.io
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo systemctl status containerd
+sudo swapoff -a
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.27/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb [trusted=yes] https://pkgs.k8s.io/core:/stable:/v1.27/deb/ /
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
